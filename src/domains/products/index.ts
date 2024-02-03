0f58428c8a3539562da0127024039caf68550831
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2024-02-03 00:34:41 -0300

    Rotas de produtos incluido a autenticação

--- src/domains/products/index.ts
+++ src/domains/products/index.ts
@@ -3,12 +3,29 @@ import { adaptRoute } from '../../adapts/adaptRoute';
 import { makeGetByIdProductsController, makeGetProductsController, makeRegisterProductsController, makeUpdateProductsController } from './factories/productsFactory';
 import { validateResource } from '../../middleware/validation';
 import { registerProductsDTO } from './validations/registerProductsDTO';
+import { makeTokenMiddleware } from '../../middleware/tokenMiddleware';
+import { adaptMiddleware } from '../../adapts/adaptMiddleware';
 
 const ProductsRoutes = Router();
 
-ProductsRoutes.post('/', validateResource(registerProductsDTO),adaptRoute(makeRegisterProductsController()));
-ProductsRoutes.get('/', adaptRoute(makeGetProductsController()));
-ProductsRoutes.get('/:id', adaptRoute(makeGetByIdProductsController()));
-ProductsRoutes.put('/:id', adaptRoute(makeUpdateProductsController()));
+ProductsRoutes.post('/',
+    adaptMiddleware(makeTokenMiddleware()),
+    validateResource(registerProductsDTO),
+    adaptRoute(makeRegisterProductsController())
+);
+
+ProductsRoutes.get('/',
+    adaptMiddleware(makeTokenMiddleware()),
+    adaptRoute(makeGetProductsController())
+);
+
+ProductsRoutes.get('/:id',    
+    adaptMiddleware(makeTokenMiddleware()),
+    adaptRoute(makeGetByIdProductsController())
+);
+
+ProductsRoutes.put('/:id',
+    adaptMiddleware(makeTokenMiddleware()), adaptRoute(makeUpdateProductsController())
+);
 
 export {ProductsRoutes};
